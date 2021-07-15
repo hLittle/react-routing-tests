5211eb949afa9a1a519cea9569536de68f09a0be
Heidi Little <hlittle@everquote.com>
2021-07-15 12:06:56 -0400

    use react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { Wrapper } from './components/Wrapper';
-import { RouteContext, RouteProvider } from './contexts/routeContext';
+import { RouteProvider } from './contexts/routeContext';
 import { Stage } from './pages/Stage';
 
 function App() {
   return (
     <RouteProvider>
       <Wrapper title="foo">
-        <RouteContext.Consumer>
-          {({ state }) => (
-            <Stage id={state.currentStage} />
-          )}
-        </RouteContext.Consumer>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/stage/:id" element={<Stage />} />
+            <Route path="/" element={<Stage />} />
+          </Routes>
+        </BrowserRouter>
       </Wrapper>
     </RouteProvider>
   );
--- src/pages/Stage.tsx
+++ src/pages/Stage.tsx
@@ -1,14 +1,18 @@
 import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import { Page } from "../components/Page";
 import { RouteContext, stageSelector } from "../contexts/routeContext";
 
-export const Stage = ({ id }: { id?: string }) => {
-  const { state, dispatch } = useContext(RouteContext);
+export const Stage = () => {
+  const { state } = useContext(RouteContext);
+  const navigate = useNavigate();
+  const { id } = useParams();
   const stage = stageSelector(state, id);
   const handleClick = () => {
-    dispatch({type: 'UPDATE_CURRENT_STAGE', payload: String(Number(id ?? 1) + 1)})
+    navigate(`/stage/${Number(id ?? 1) + 1}`)
   }
-  return (<Page title={`Page ${stage?.name}`}>
+  return (
+    <Page title={`Page ${stage?.name}`}>
       <button type="submit" onClick={handleClick}>Submit</button>
     </Page>
   )
