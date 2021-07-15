1361d46e47e301575dee8fd9899f1eea8956164e
Heidi Little <hlittle@everquote.com>
2021-07-15 12:15:43 -0400

    add router switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { ComponentType } from 'react';
+import { Route, Routes, BrowserRouter, HashRouter, MemoryRouter } from 'react-router-dom';
 import './App.css';
 import { Wrapper } from './components/Wrapper';
 import { RouteProvider } from './contexts/routeContext';
 import { Stage } from './pages/Stage';
 
 function App() {
+  let config = 'static';
+  let Router: ComponentType;
+  switch (config) {
+    case 'hash':
+      Router = HashRouter;
+      break;
+    case 'static':
+      Router = MemoryRouter;
+      break;
+    default:
+      Router = BrowserRouter;
+      break;
+  }
   return (
     <RouteProvider>
       <Wrapper title="foo">
-        <BrowserRouter>
+        <Router>
           <Routes>
             <Route path="/stage/:id" element={<Stage />} />
             <Route path="/" element={<Stage />} />
           </Routes>
-        </BrowserRouter>
+        </Router>
       </Wrapper>
     </RouteProvider>
   );
