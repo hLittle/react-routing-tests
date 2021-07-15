a18a03f6987a325d3aa607e45d3f7bd13acf8dcf
Heidi Little <hlittle@everquote.com>
2021-07-15 12:35:14 -0400

    update to useRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { ComponentType } from 'react';
-import { Route, Routes, BrowserRouter, HashRouter, MemoryRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter, MemoryRouter } from 'react-router-dom';
 import './App.css';
 import { Wrapper } from './components/Wrapper';
 import { RouteProvider } from './contexts/routeContext';
-import { Stage } from './pages/Stage';
+
+
 
 function App() {
-  let config = 'static';
+  let config = 'path';
   let Router: ComponentType;
   switch (config) {
     case 'hash':
@@ -19,16 +20,12 @@ function App() {
       Router = BrowserRouter;
       break;
   }
+  
   return (
     <RouteProvider>
-      <Wrapper title="foo">
-        <Router>
-          <Routes>
-            <Route path="/stage/:id" element={<Stage />} />
-            <Route path="/" element={<Stage />} />
-          </Routes>
-        </Router>
-      </Wrapper>
+      <Router>
+       <Wrapper title="foo" />
+      </Router>
     </RouteProvider>
   );
 }
--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,7 +1,14 @@
 import { PropsWithChildren } from "react";
+import { useRoutes } from "react-router-dom";
+import { Stage } from "../pages/Stage";
 
-export const Wrapper = ({ children }: PropsWithChildren<{ title: string }>) => (
+export const Wrapper = ({ children }: PropsWithChildren<{ title: string }>) => {
+  let element = useRoutes([
+    { path: '/stage/:id', element: <Stage />},
+    { path: '/', element: <Stage /> }
+  ])
+  return (
   <div style={{ width: '600px', height: '90vh', margin: '20px auto auto auto', textAlign: 'center', border: '1px solid grey' }}>
-    {children}
+    {element}
   </div>
-)
+)}
