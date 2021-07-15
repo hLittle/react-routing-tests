50bcc0eb959acda77db719eafc1ed6924585ad1e
Heidi Little <hlittle@everquote.com>
2021-07-15 13:47:04 -0400

    add test hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
-import { Route, Router } from 'wouter';
+import { useEffect, useState } from 'react';
+import { BaseLocationHook, Route, Router } from 'wouter';
 import './App.css';
 import { Wrapper } from './components/Wrapper';
 import { RouteProvider } from './contexts/routeContext';
 import { Stage } from './pages/Stage';
 
+// returns the current hash location in a normalized form
+// (excluding the leading '#' symbol)
+const currentLocation = () => {
+  return window.location.hash.replace(/^#/, "") || "/";
+};
+
+const navigate = (to: string) => (window.location.hash = to);
+
+const useHashLocation: BaseLocationHook = () => {
+  const [loc, setLoc] = useState(currentLocation());
+
+  useEffect(() => {
+    // this function is called whenever the hash changes
+    const handler = () => setLoc(currentLocation());
+
+    // subscribe to hash changes
+    window.addEventListener("hashchange", handler);
+    return () => window.removeEventListener("hashchange", handler);
+  }, []);
+
+  return [loc, navigate];
+};
+
 function App() {
+  let config = 'hash';
+  let hook: BaseLocationHook | undefined;
+  switch (config) {
+    case 'hash':
+      hook = useHashLocation;
+      break;
+    default:
+      hook = undefined;
+      break;
+  }
   return (
     <RouteProvider>
       <Wrapper title="foo">
-        <Router>
+        <Router hook={hook}>
           <Route path="/stage/:id">{(params) => <Stage id={params.id} />}</Route>
           <Route><Stage /></Route>
         </Router>
--- src/contexts/routeContext.tsx
+++ src/contexts/routeContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, Dispatch, PropsWithChildren, useReducer } from "react";
+import { createContext, Dispatch, PropsWithChildren, useEffect, useReducer } from "react";
+import { useLocation } from "wouter";
 
 interface Stage {
   name: string,
@@ -59,6 +60,11 @@ export const RouteContext = createContext({
 
 export const RouteProvider = ({ children }: PropsWithChildren<{}>) => {
   const [state, dispatch] = useReducer(routeReducer, initialState);
+  const [location] = useLocation();
+
+  useEffect(() => {
+    dispatch({ type: 'UPDATE_CURRENT_STAGE', payload: location})
+  }, [location])
 
   return (
     <RouteContext.Provider value={{ state, dispatch }}>
